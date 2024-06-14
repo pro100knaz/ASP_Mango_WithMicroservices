a3f21eeb69da34e32c116e5f5d572bc15af3a7d6
MFlEGENDANTON <jffgx@mail.ru>
2024-06-14 17:35:45 +0300

    подключение базы данных для облектов в корзине

--- Mango.Services.ShopingCartApi/Program.cs
+++ Mango.Services.ShopingCartApi/Program.cs
@@ -1,3 +1,11 @@
+using AutoMapper;
+using Mango.Services.ShopingCartApi;
+using Mango.Services.ShopingCartApi.Data;
+using Mango.Services.ShopingCartApi.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +13,50 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme()
+	{
+		Name = "Authorization",
+		Description = "Enter the Beare Authorization string as following:  'Bearer GEnerated-JWT-Token'",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	}); // default vaalue to set authentification for swagger
+
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id= JwtBearerDefaults.AuthenticationScheme
+				}
+			},
+			new string[]{}
+		}
+	});
+});
+
+
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.AddAppAuthentication();
+
+builder.Services.AddAuthentication();
 
 var app = builder.Build();
 
