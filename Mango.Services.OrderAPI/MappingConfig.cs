da96c8ea196334af03630d2a3adc61c46ad1a80b
MFlEGENDANTON <jffgx@mail.ru>
2024-06-16 18:38:15 +0300

    BaseSetup for OrderApi

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.DTO;
+
+
+
+namespace Mango.Services.ShopingCartApi
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config =>
+			{
+				config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+				config.CreateMap<OrderHeaderDto, OrderHeader>().ReverseMap();
+
+
+			});
+
+			return mappingConfig;
+		}
+	}
+}
